11a0789adcfc7fbb8e2e0378025be55e6c22daad
Oriol Perarnau <oriol.perarnau@estudiantat.upc.edu>
2025-02-27 15:50:23 +0100

    No se m'havien guardat alguns codis

--- src/codi_1.cpp
+++ src/codi_1.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-#define LED_BUILTIN 2      // Pin del LED (canvia si cal)
-#define DELAY 500 // Temps de retard en mil·lisegons
+
+#define LED_PIN 48  // Canvia al pin correcte si cal
+#define NUM_LEDS 1  // Si només tens un LED NeoPixel
+#define DELAY_TIME 500 // Temps en mil·lisegons
+
+
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);   // Configurar el LED com a sortida
-    Serial.begin(115200);       // Iniciar la comunicació sèrie
+    Serial.begin(115200);
+    strip.begin();
+    strip.show();
+    Serial.println("ESP32-S3 iniciat, NeoPixel parpellejant en blanc...");
 }
 
+
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(DELAY);
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("ON");
-    delay(DELAY);
-    Serial.println("OFF");        
+    // Encén el LED en blanc (RGB: 255, 255, 255)
+    strip.setPixelColor(0, strip.Color(255, 255, 255));
+    strip.show();
+    Serial.println("NeoPixel ENCÈS!");
+    delay(DELAY_TIME);
+
+
+    // Apaga el LED
+    strip.setPixelColor(0, strip.Color(0, 0, 0));
+    strip.show();
+    Serial.println("NeoPixel APAGAT!");
+    delay(DELAY_TIME);
 }
+
